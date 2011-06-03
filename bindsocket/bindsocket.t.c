1431681e8c042a02f3d5cfcc1add395235a25d57
Glenn Strauss <gs-github.com@gluelogic.com>
2011-06-03 03:15:59 +0000

    bindsocket.t.c - commit code for simple load test (serial requests)

--- bindsocket/bindsocket.t.c
+++ bindsocket/bindsocket.t.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include <bindsocket_addrinfo.h>
 #include <bindsocket_bind.h>
@@ -53,10 +54,33 @@ main (int argc, char *argv[])
     aistr.service  = argv[4];
     aistr.addr     = argv[5];
 
+  #if 1
     if (        bindsocket_addrinfo_from_strs(&ai, &aistr)
         &&-1 != (nfd = socket(ai.ai_family, ai.ai_socktype, ai.ai_protocol))
         && 0 == bindsocket_bind_addrinfo(nfd, &ai))
         return EXIT_SUCCESS;
+  #else  /* load test (serial requests) */
+    int i;
+    struct timeval tva, tvb;
+    bindsocket_addrinfo_from_strs(&ai, &aistr);
+    gettimeofday(&tva, NULL);
+    for (i=0; i<10000; ++i) {
+        if (-1 == (nfd = socket(ai.ai_family, ai.ai_socktype, ai.ai_protocol))
+            || 0 != bindsocket_bind_addrinfo(nfd, &ai))
+            break;
+        close(nfd);
+    }
+    gettimeofday(&tvb, NULL);
+    time_t secs = tvb.tv_sec - tva.tv_sec;
+    long usecs = tvb.tv_usec - tva.tv_usec;
+    if (usecs < 0) {
+        usecs = tva.tv_usec - tvb.tv_usec;
+        --secs;
+    }
+    fprintf(stderr, "count: %d %u:%u\n", i, (unsigned)secs,(unsigned)usecs);
+    if (i == 10000)
+        return EXIT_SUCCESS;
+  #endif
 
     perror("bindsocket");
     return EXIT_FAILURE;
